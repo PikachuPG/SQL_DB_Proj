ce721986dc3630c497c1bdc0b855f0da82092c42
Prakhar Gupta <99659864+PikachuPG@users.noreply.github.com>
2026-01-31 22:45:30 +0530

    Add files via upload
    Schema creation of various layers namely, bronze, silver & gold for data extraction, cleaning and analysis

---
+++ Schema Generation.sql
@@ -0,0 +1,19 @@
+USE MASTER;
+GO
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+BEGIN 
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE Datawarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+USE DataWarehouse;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
